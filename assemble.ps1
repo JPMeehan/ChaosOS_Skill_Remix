7796ec5607d7548ed65d363cf59b9571343f5801
Joseph <JPMeehan5@Gmail.com>
2021-06-01 22:32:59 -0400

    Rebuild
    Rebuilt project

--- assemble.ps1
+++ assemble.ps1
@@ -1,11 +1,11 @@
-$module = Get-Content .\module.json | ConvertFrom-Json
+$module = Get-Content .\Module\module.json | ConvertFrom-Json
 $version = $module.version
-lessc "chaosos-skill-remix\chaosos-skill.less" "chaosos-skill-remix\chaosos-skill.css"
-if (test-path .\archive\$version\chaosos-skill-remix.zip) {
-    Remove-Item .\archive\$version\chaosos-skill-remix.zip
+lessc "Module\chaosos-skill.less" "Module\chaosos-skill.css"
+if (test-path .\chaosos-skill-remix.zip) {
+    Remove-Item .\chaosos-skill-remix.zip
     Write-Output "Deleted old file"
 }
-Compress-Archive -Path .\chaosos-skill-remix -DestinationPath .\archive\$version\chaosos-skill-remix.zip
-Copy-Item -Path .\LICENSE -Destination .\archive\$version
-Copy-Item -Path .\README.md -Destination .\archive\$version
+Compress-Archive -Path .\Module -DestinationPath .\chaosos-skill-remix.zip
+# Copy-Item -Path .Module\LICENSE -Destination .\archive\$version
+# Copy-Item -Path .Module\README.md -Destination .\archive\$version
 Write-Output "Freshened up the project to version $version"
