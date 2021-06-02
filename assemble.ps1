81bd32e9c58bf929740cd31070d1621d2cb1c8bc
Joseph <JPMeehan5@Gmail.com>
2021-06-01 23:06:31 -0400

    Shuffling
    Re-wrapping my personal code section

--- assemble.ps1
+++ assemble.ps1
@@ -1,11 +1,11 @@
-$module = Get-Content .\Module\module.json | ConvertFrom-Json
+$module = Get-Content .\module.json | ConvertFrom-Json
 $version = $module.version
-lessc "Module\chaosos-skill.less" "Module\chaosos-skill.css"
+lessc "chaosos-skill.less" "chaosos-skill.css"
 if (test-path .\chaosos-skill-remix.zip) {
     Remove-Item .\chaosos-skill-remix.zip
     Write-Output "Deleted old file"
 }
-Compress-Archive -Path .\Module -DestinationPath .\chaosos-skill-remix.zip
+Compress-Archive -Path .\ -DestinationPath .\chaosos-skill-remix.zip
 # Copy-Item -Path .Module\LICENSE -Destination .\archive\$version
 # Copy-Item -Path .Module\README.md -Destination .\archive\$version
 Write-Output "Freshened up the project to version $version"
