ff1d149bd0e25d6e710adec6e01362eb272afa09
Joseph <JPMeehan5@Gmail.com>
2022-07-15 22:03:38 -0700

    Fucking around and finding out

--- chaosos-skill.js
+++ chaosos-skill.js
@@ -9,22 +9,26 @@ Hooks.once("setup", () => {
     patchActor5eRollSkill();
 });
 
-removed_skills = ["dec","itm","med","per","prf"];
 
-new_skill = {
-    "app": { // Appeal
-        "value": 0,
-        "ability": "cha"
-    },
-    "deb": { // Debate
-        "value": 0,
-        "ability": "int"
-    },
-    "imp": { // Impress
-        "value": 0,
-        "ability": "cha"
-    }
-}
+
+Hooks.once('setup', async function dnd5eExtenderSetup() {
+    console.log(MODULE_ID, '|', `Initializing ${MODULE_ID}`);
+
+    // Set a class name on the body so our css overrides will take effect
+    // $('body').addClass('dnd5e-extender');
+
+    // registerRenderActorSheet5eHook()
+    // patchActor5e_prepareDerivedData();
+    // patchEntityCollection_importFromCollection();
+    // registerSettings();
+    addCustomConfig();
+
+    // Load Handlebars templates last as it is comparatively expensive and
+    // we need to beat dnd5e starting to set up
+    await loadTemplates(Object.values(flattenObject(TEMPLATES)));
+
+    Hooks.call(`DND5eExtenderReady`);
+});
 
 function patchActor5ePrepareData() {
     libWrapper.register(MODULE_NAME, "CONFIG.Actor.entityClass.prototype.prepareData", function patchedPrepareData(wrapped, ...args) {
@@ -33,6 +37,28 @@ function patchActor5ePrepareData() {
         const skills = this.data.data.skills;
         
     }, "WRAPPER");
+}
+
+function addCustomConfig() {
+    const customAbilities = game.settings.get(MODULE_ID, SETTINGS.CUSTOM_ABILITIES);
+    for (let { abbreviation, title } of customAbilities) {
+        CONFIG.DND5E.abilities[abbreviation] = title;
+        CONFIG.DND5E.abilityAbbreviations[abbreviation] = abbreviation;
+
+        // Inject the label and abbreviation for each custom ability as a translation
+        const capitalizedAbbr = abbreviation.charAt(0).toUpperCase() + abbreviation.slice(1);
+        setProperty(game.i18n.translations.DND5E, `Ability${capitalizedAbbr}`, title);
+        setProperty(game.i18n.translations.DND5E, `Ability${capitalizedAbbr}Abbr`, abbreviation);
+    }
+
+    const customSkills = game.settings.get(MODULE_ID, SETTINGS.CUSTOM_SKILLS);
+    for (let { abbreviation, title } of customSkills) {
+        CONFIG.DND5E.skills[abbreviation] = title;
+
+        // Inject the title for each custom skill as a translation
+        const capitalizedAbbr = abbreviation.charAt(0).toUpperCase() + abbreviation.slice(1);
+        setProperty(game.i18n.translations.DND5E, `Skill${capitalizedAbbr}`, title);
+    }
 }
 
 function patchActor5eRollSkill() {}
---
+++ scripts/constants.js
@@ -0,0 +1,42 @@
+export const MODULE_ID = 'chaosos-skill-remix';
+export const MODULE_ABBREV = 'CSKRMX';
+
+// export const TEMPLATES = {
+//     settings: `modules/${MODULE_ID}/templates/settings.hbs`,
+//     settingSubmit: `modules/${MODULE_ID}/templates/settings-submit.hbs`,
+//     abilities: {
+//         table: `modules/${MODULE_ID}/templates/abilities/settings-abilities.hbs`,
+//         tableRow: `modules/${MODULE_ID}/templates/abilities/settings-abilities-tr.hbs`,
+//     },
+//     skills: {
+//         table: `modules/${MODULE_ID}/templates/skills/settings-skills.hbs`,
+//         tableRow: `modules/${MODULE_ID}/templates/skills/settings-skills-tr.hbs`,
+//     },
+// };
+
+// export const SETTINGS = {
+//     CUSTOM_ABILITIES: "custom-abilities",
+//     CUSTOM_SKILLS: "custom-skills",
+// };
+
+// export const FLAGS = {
+//     SAVED_ABILITIES: "saved-abilities",
+//     SAVED_SKILLS: "saved-skills",
+// }
+
+export const REMOVED_SKILLS = ["dec","itm","med","per","prf"];
+
+export const NEW_SKILLS = {
+    "app": { // Appeal
+        "value": 0,
+        "ability": "cha"
+    },
+    "deb": { // Debate
+        "value": 0,
+        "ability": "int"
+    },
+    "imp": { // Impress
+        "value": 0,
+        "ability": "cha"
+    }
+}
